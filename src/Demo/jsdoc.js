17454095b710f24a12504c074d3877104e8ba4ab
janlin002 <123davidbill@gmail.com>
2022-09-06 11:14:58 +0800

    jsdoc

---
+++ src/Demo/jsdoc.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const JsDoc = () =>{
+    /**
+       * @param {numbrt} a 參數1
+       * @param {number} b 參數2
+       * @param {number} c 參數3
+       * @returns {number|boolean} 相加
+       */
+    const handleClick = (a, b, c) =>{
+      if(c){
+        return a+b>c
+      }
+      return a + b
+    }
+    return (
+      <div>
+        <button 
+          onClick={handleClick(1, 2, 3)}
+        >點擊</button>
+      </div>
+    )
+  }
+
+export default JsDoc
