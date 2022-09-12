d0f9c1ca882879cc287c6c5fdc5d5ee56109b788
janlin002 <123davidbill@gmail.com>
2022-09-12 20:02:33 +0800

    delete console

--- src/Demo/jodit-react.js
+++ src/Demo/jodit-react.js
@@ -10,16 +10,13 @@ function JoditReact() {
     placeholder: content || 'Start typings...',
   }
 
-  //   console.log(content, 'content')
   return (
     <>
       <JoditEditor
         ref={editor}
         value={content}
         config={config}
-    //   tabIndex={1} // tabIndex of textarea
         onBlur={(newContent) => setContent(newContent)}
-        // onChange={(newContent) => {}}
       />
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </>
